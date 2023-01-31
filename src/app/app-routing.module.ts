dff3f823f591520a47b6ebc2b22588e22a211e4c
thulisile98 <thulisile.ma98@gmail.com>
2023-01-31 15:56:55 +0200

    generated home component, added router configuration,  added router outlet and food images in assets generated a service, addeed a method in the servic , injected the ervice in home component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
 
   {
     path: '',
-    component: HeaderComponent,
+    component: HomeComponent,
   }
 ];
 
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FoodService } from '../services/food/food.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
 
+  foods: string[] = [];
+  constructor(private fs: FoodService) {
+
+  }
+  ngOnInit() {
+    this.foods = this.fs.getAll();
+
+  }
 }
