8acca8412fae1ff86e0d8a00df9e7e66a3ca34c0
thulisile98 <thulisile.ma98@gmail.com>
2023-01-31 23:43:45 +0200

    changed all getAll method in food service, created food class

---
+++ src/app/shared/food.ts
@@ -0,0 +1,14 @@
+export class Foods {
+
+    id!: number;
+    price!: number;
+    name!: string;
+    favourite: boolean = false;
+    star: number = 0;
+    tags?: string[];
+    imageUrl!: string;
+    cookTime!: string;
+    original!: string[];
+
+
+}
