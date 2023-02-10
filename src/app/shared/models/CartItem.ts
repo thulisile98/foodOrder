7d906b983d478736cd4bcb810ceea083c5884dab
thulisile98 <thulisile.ma98@gmail.com>
2023-02-10 17:13:35 +0200

    generated cart services

--- src/app/shared/models/CartItem.ts
+++ src/app/shared/models/CartItem.ts
@@ -2,8 +2,18 @@ import { Foods } from './../food';
 
 
 export class CartItem {
+    [x: string]: number;
 
-    // constructor(food: Foods) { }
-    // food: Foods;
-    // quantity: number = 1;
+    constructor(food: Foods) {
+        this.food = food;
+
+    }
+
+
+    food: Foods;
+    quantity: number = 1;
+    getPrice(): Number {
+
+        return this.food.price * this.quantity
+    }
 }
