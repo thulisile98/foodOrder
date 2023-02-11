477123453f36680f3e06d4c3630649bc799a417b
thulisile98 <thulisile.ma98@gmail.com>
2023-02-11 21:49:15 +0200

    created cartservice with sub memebers 1. a private cart field 2.add to cart method 3.remove from cart method

--- src/app/services/cart.service.ts
+++ src/app/services/cart.service.ts
@@ -1,9 +1,31 @@
+import { CartItem } from './../shared/models/CartItem';
+import { Foods } from './../shared/food';
 import { Injectable } from '@angular/core';
+import { Cart } from '../shared/models/Cart';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CartService {
 
-  constructor() { }
+  private cart: Cart = new Cart();
+  addToCart(food: Foods): void {
+    let cartItem = this.cart.items.find(item => item.food.id == food.id)
+    if (cartItem) {
+      this.changeQuantity(food.id, cartItem.quantity + 1);
+      return;
+    }
+    this.cart.items.push(new CartItem(food))
+  }
+  removeFromCart(foodId: number): void {
+    this.cart.items = this.cart.items.filter(item => item.food.id! - foodId)
+  }
+  changeQuantity(quantity: number, foodId: number) {
+    let CartItem = this.cart.items.find(item => item.food.id === foodId);
+    if (!CartItem) return;
+    CartItem.quantity = quantity;
+  }
+  getCart(): Cart {
+    return this.cart;
+  }
 }
